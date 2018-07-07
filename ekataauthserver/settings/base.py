f40cc5368996e8de5175772b1b1cebcafe894764
prasanta <prasantakakati1994@gmail.com>
2018-07-08 01:58:45 +0530

    [FIXED] Multiple account creation if email id associated with other account

--- ekataauthserver/settings/base.py
+++ ekataauthserver/settings/base.py
@@ -183,6 +183,18 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # Social Auth
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'authhelper.pipeline.check_email_exists',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 SOCIAL_AUTH_FACEBOOK_KEY = get_env_var('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = get_env_var('SOCIAL_AUTH_FACEBOOK_SECRET')
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
